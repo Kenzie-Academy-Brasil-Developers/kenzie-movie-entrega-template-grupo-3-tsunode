ea6aac0cd31a8ad829d438b2444617ee8db2b29a
DenisVitor <denisvitor41@gmail.com>
2023-07-03 10:59:10 -0300

    .

---
+++ src/styles/Global.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalVariables = createGlobalStyle`
+    :root{
+        --black: #000;
+        --white: #fff;
+        --lightgray1: #3d3d3d;
+        --lightgray2: #171717;
+        --yellow: #ffbb38;
+        --ff: 'Poppins', sans-serif;
+    }
+
+`
