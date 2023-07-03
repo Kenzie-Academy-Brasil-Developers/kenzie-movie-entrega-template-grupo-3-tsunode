6211ac9cb5631db490872970af5fd07329e01fce
Laroy Prado <laroyprado@gmail.com>
2023-07-03 13:52:08 -0300

    Alterações no git ignore

---
+++ src/Components/Register/Register.tsx
@@ -0,0 +1,11 @@
+export const Register = () =>{
+    return(
+        <form >
+            <div>
+            <h1>Cadastro</h1>
+            <button>  Voltar</button>
+            </div>
+            <p>Preencha os campos para cadastrar-se</p>
+        </form>
+    )
+}
