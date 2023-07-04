54376fc805d87a76ffaa4e972ca73adfab4e1d64
Thiago <thiagomesquita_mat@outlook.com>
2023-07-04 10:32:15 -0300

    Criação das funcionalidades de login

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,32 @@
 import { Link } from "react-router-dom";
+import { Footer } from "../../Components/Footer/Foot";
+import { useForm } from "react-hook-form";
+import { api } from "../../services/api";
 
 export const LoginPage = () => {
     
+    const { register, handleSubmit } = useForm<FormData>()
 
+    interface FormData {
+        email: string;
+        password: string;
+    }
 
+    const submit = (formData: FormData) => {
+        console.log(formData)
+        loginUser(formData)
+    }
+
+    const loginUser = async (formData: FormData) => {
+        try {
+            const { data } = await api.post('/login', formData)
+            localStorage.setItem('@TOKEN', JSON.stringify(data.accessToken))
+            localStorage.setItem('@USERID', JSON.stringify(data.user.id))
+        } catch (error) {
+            console.log('233')
+
+        }
+    } 
     
     return(
         <>
@@ -12,16 +35,15 @@ export const LoginPage = () => {
             <a href="/loginPage">loginPage</a>
             <a href="/registerPage">registerPage</a>
             <header></header>
-            <form action="">
+            <form onSubmit={handleSubmit(submit)}>
                 <h1>Login</h1>
-                <input type="email" placeholder="E-mail" />
-                <input type="text" placeholder="senha" />
+                <input type="email" placeholder="E-mail" {...register('email')} />
+                <input type="text" placeholder="senha" {...register('password')} />
                 <button>Entrar</button>
                 <p>ou</p>
                 <Link to={'/registerPage'}>Cadastre-se</Link>
-                
             </form>
-            <footer></footer>
+            <Footer/>
         
         
         </>
