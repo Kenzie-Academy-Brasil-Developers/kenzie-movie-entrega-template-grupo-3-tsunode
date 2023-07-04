67e28944b7117f015ad33cb20ae74bb2d4b2af2c
DenisVitor <denisvitor41@gmail.com>
2023-07-04 16:09:50 -0300

    .

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -1,53 +1,22 @@
 import { Footer } from "../../Components/Footer/Foot";
 // import { LoginForm } from "../../Components/LoginForm";
-import { api } from "../../services/api";
-import { Link } from "react-router-dom";
 import { LoginLowerSection, StyledLoginForm, StyledLoginSection } from "./LoginStyle";
 import { StyledInput } from "../../styles/Inputs";
 import { Paragraph, RegisterLink, Title1 } from "../../styles/typography";
 import { MediumYellowButton } from "../../styles/Buttons";
+import { LoginForm } from "../../Components/LoginForm";
 
 
 export const LoginPage = () => {
     
-    const { register, handleSubmit } = useForm<FormData>()
-
-    interface FormData {
-        email: string;
-        password: string;
-    }
-
-    const submit = (formData: FormData) => {
-        console.log(formData)
-        loginUser(formData)
-    }
-
-    const loginUser = async (formData: FormData) => {
-        try {
-            const { data } = await api.post('/login', formData)
-            localStorage.setItem('@TOKEN', JSON.stringify(data.accessToken))
-            localStorage.setItem('@USERID', JSON.stringify(data.user.id))
-        } catch (error) {
-            console.log('233')
-
-        }
-    } 
-    
     return(
         <>
         
-            <a href="/homePage">homePage</a>
+            <a href="/">homePage</a>
             <a href="/loginPage">loginPage</a>
             <a href="/registerPage">registerPage</a>
             <header></header>
-            <form onSubmit={handleSubmit(submit)}>
-                <h1>Login</h1>
-                <input type="email" placeholder="E-mail" {...register('email')} />
-                <input type="text" placeholder="senha" {...register('password')} />
-                <button>Entrar</button>
-                <p>ou</p>
-                <Link to={'/registerPage'}>Cadastre-se</Link>
-            </form>
+            <LoginForm />
             <Footer/>
         
         
