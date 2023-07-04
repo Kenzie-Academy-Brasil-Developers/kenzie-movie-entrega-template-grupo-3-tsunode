9b2c1f8d2b83e123604a1591f76e14a92f3fe5c7
Laroy Prado <laroyprado@gmail.com>
2023-07-04 11:06:52 -0300

    Página home agora é a padrão com "/"

--- src/routes/RoutesMainTest.tsx
+++ src/routes/RoutesMainTest.tsx
@@ -1,20 +1,16 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from '../Pages/HomePage/HomePage';
-import { LoginPage } from '../Pages/LoginPage/LoginPage';
-import { RegisterPage } from '../Pages/RegisterPage/RegisterPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "../Pages/HomePage/HomePage";
+import { LoginPage } from "../Pages/LoginPage/LoginPage";
+import { RegisterPage } from "../Pages/RegisterPage/RegisterPage";
 
 export const RoutesMain = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/homePage" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/loginPage" element={<LoginPage />} />
         <Route path="/registerPage" element={<RegisterPage />} />
       </Routes>
     </Router>
   );
 };
-
-
-
