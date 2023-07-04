65e7f75aaa2a9f12d984400656d200f6f0582730
Laroy Prado <laroyprado@gmail.com>
2023-07-04 11:30:27 -0300

    Componente Input Com Estilização

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,10 +1,11 @@
 import { InputHTMLAttributes } from "react";
 import { UseFormRegisterReturn } from "react-hook-form";
+import { StyledInput } from "../../styles/Inputs";
 
 interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
   register: UseFormRegisterReturn;
 }
 
 export const Input = ({ register, ...rest }: IInputProps) => {
-  return <input {...register} {...rest} />;
+  return <StyledInput {...register} {...rest} />;
 };
