7984aba5ca2c5a14a493f20a886e8792ca0143da
Laroy Prado <laroyprado@gmail.com>
2023-07-04 09:46:52 -0300

    Adicionando função de registro

--- src/Components/Register/Register.tsx
+++ src/Components/Register/Register.tsx
@@ -1,20 +1,37 @@
-import arrow from "../../assets/arrow.svg"
-import { Button } from "../Button/Button"
-import { Input } from "../Input/Input"
+import arrow from "../../assets/arrow.svg";
+import { Button } from "../Button/Button";
+import { Input } from "../Input/Input";
+import { SubmitHandler, useForm } from "react-hook-form";
 
-export const Register = () =>{
-    return(
-        <form >
-            <div>
-            <h1>Cadastro</h1>
-            <button> <img src={arrow} alt="" />  Voltar</button>
-            </div>
-            <p>Preencha os campos para cadastrar-se</p>
-            <Input placeholder="Nome" />
-            <Input placeholder="E-mail" />
-            <Input placeholder="Senha" />
-            <Input placeholder="Confirmar Senha" />
-            <Button title="Cadastre-se" />
-        </form>
-    )
+interface IRegisterUser {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
 }
+
+export const Register = () => {
+  const { register, handleSubmit } = useForm<IRegisterUser>();
+
+  const onSubmit: SubmitHandler<IRegisterUser> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <h1>Cadastro</h1>
+        <button>
+          {" "}
+          <img src={arrow} alt="" /> Voltar
+        </button>
+      </div>
+      <p>Preencha os campos para cadastrar-se</p>
+      <Input placeholder="Nome" {...register("name")} />
+      <Input placeholder="E-mail" {...register("email")} />
+      <Input placeholder="Senha" {...register("password")} />
+      <Input placeholder="Confirmar Senha" {...register("confirmPassword")} />
+      <Button title="Cadastre-se" />
+    </form>
+  );
+};
