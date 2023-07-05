3a6831df68f4c217872da5e1f03f1f2a22fff299
Vitor Augusto Vasconcelos <124096321+VitorAugustoVasconcelos@users.noreply.github.com>
2023-07-05 08:23:02 -0300

    renderização das Avaliações na RenderPage

--- src/Pages/RenderPage/RenderPage.tsx
+++ src/Pages/RenderPage/RenderPage.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect} from 'react';
+import { useContext, useEffect } from 'react';
 import { IMovies } from '../../Pages/HomePage/HomePage';
 import { MovieContext } from '../../providers/MovieContext';
 import { StyledUpperSection } from '../../Components/MoviesCard/MoviesCardStyle';
@@ -7,48 +7,86 @@ import { Paragraph, Title2 } from '../../styles/typography';
 import { api } from '../../services/api';
 
 export const RenderPage = () => {
-  
   const { setSelectMovie, selectMovie } = useContext(MovieContext) as {
     setSelectMovie: (movie: IMovies | null) => void;
     selectMovie: (movie: IMovies | null) => void;
-  }
-
- 
-
+  };
   useEffect(() => {
     const loadMovie = async () => {
-        const movieId = localStorage.getItem('@LOCALMOVIEID')
-        try {
-            const { data } = await api.get(`/movies/${movieId}?_embed=reviews`)
-            setSelectMovie(data)
-        } catch (error) {
-            console.log(error.message)
+      const movieId = localStorage.getItem('@LOCALMOVIEID');
+      try {
+        const { data } = await api.get(`/movies/${movieId}?_embed=reviews`);
+        setSelectMovie(data);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    loadMovie();
+    setSelectMovie(null);
+  }, []);
+
+  console.log(selectMovie);
+
+  const fetchReviews = async () => {
+    const movieId = localStorage.getItem('@LOCALMOVIEID');
+    try {
+      const { data } = await api.get(`/movies/${movieId}?_embed=reviews`);
+
+      const reviews = data.reviews.map((review: any) => {
+        console.log(review.movieId);
+        console.log(review.movieId);
+        console.log(review.score);
+        console.log(review.description)
+        return {
+          movieId: review.movieId,
+          userId: review.userId,
+          score:review.score,
+          description:review.description
         }
+      });
+      console.log(reviews)
+    } catch (error) {
+      console.log(error)
     }
-    loadMovie()
-    setSelectMovie(null)
-}, [])
-
-  console.log(selectMovie)
-
+  };
+  fetchReviews();
 
   return (
     <>
-      {selectMovie == null ? <div>loading</div> : <div><img src={selectMovie.image} />
-      <StyledUpperSection>
-        <SmallYellowButton buttonSize={10}>{selectMovie.type}</SmallYellowButton>
-        <Paragraph>{selectMovie.duration}</Paragraph>
-      </StyledUpperSection>
-      <StyledUpperSection>
-        <Title2>{selectMovie.name}</Title2>
-        <Paragraph>colocar o score</Paragraph> 
-      </StyledUpperSection>
-      <Paragraph>{selectMovie.synopsis}</Paragraph></div>}
-    
-      
-    
-      
+      {selectMovie == null ? (
+        <div>loading</div>
+      ) : 
+        <div>
+          <img src={selectMovie.image} />
+          <StyledUpperSection>
+            <SmallYellowButton buttonSize={10}>
+              {selectMovie.type}
+            </SmallYellowButton>
+            <Paragraph>{selectMovie.duration}</Paragraph>
+          </StyledUpperSection>
+          <StyledUpperSection>
+            <Title2>{selectMovie.name}</Title2>
+            <Paragraph>colocar o score</Paragraph>
+          </StyledUpperSection>
+          <Paragraph>{selectMovie.synopsis}</Paragraph>
+          <div>
+            <div>
+              <Paragraph>Avaliações</Paragraph>
+              <button>Avaliar</button>
+            </div>
+           {selectMovie.reviews.map((review, index) => (
+              <div key={index}>
+                <Paragraph>{review.movieId}</Paragraph>
+                <Paragraph>{review.userId}</Paragraph>
+                <Paragraph>{review.description}</Paragraph>
+                <Paragraph>{review.score}</Paragraph>
+              </div>
+            )
+           )}
+          </div>
+        </div>
+        
+      }
     </>
   );
 };
-
